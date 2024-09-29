c9cf461486fc920f76c169e38013faa070f5063e
romain-navarro38 <romain.navarro38@gmail.com>
2024-09-29 17:16:39 +0200

    Add _setuo_ui_translation()

--- mastermind/views/help.py
+++ mastermind/views/help.py
@@ -1,17 +1,19 @@
+from PySide6.QtGui import QIcon, QPixmap
 from PySide6.QtWidgets import QWidget, QTextBrowser, QVBoxLayout, QScrollArea
 
-from mastermind.utils.parameters import get_resource, get_help, DIRECTORIES, View
+from mastermind.utils.parameters import get_resource, DIRECTORIES
 from mastermind.views.custom_widget import CustomButton
 
 
 class HelpWindow(QWidget):
     """Fenêtre d'affichage de l'aide"""
-    def __init__(self) -> None:
+    def __init__(self, translation: dict) -> None:
         super().__init__()
 
+        self.translation = translation
         self._setup_ui()
         self.resize(600, 500)
-        self.setWindowTitle("Aide")
+        self.setWindowIcon((QIcon(QPixmap(DIRECTORIES['icon'] / "logo.png"))))
         self.setStyleSheet(get_resource(DIRECTORIES['style'] / "help.qss"))
 
     def _setup_ui(self) -> None:
@@ -20,14 +22,14 @@ class HelpWindow(QWidget):
         self._setup_ui_create_layouts()
         self._setup_ui_add_widgets_to_layouts()
         self._setup_ui_connections()
+        self._setup_ui_translation()
 
     def _setup_ui_create_widgets(self) -> None:
         self.scroll_area = QScrollArea()
         self.text_browser = QTextBrowser()
-        self.btn_close = CustomButton("Fermer")
 
     def _setup_ui_modify_widgets(self) -> None:
-        self.text_browser.setText(get_help(View.WINDOW))
+        pass
 
     def _setup_ui_create_layouts(self) -> None:
         self.main_layout = QVBoxLayout(self)
@@ -40,3 +42,8 @@ class HelpWindow(QWidget):
 
     def _setup_ui_connections(self) -> None:
         self.btn_close.clicked.connect(self.close)
+
+    def _setup_ui_translation(self) -> None:
+        self.setWindowTitle(self.translation['window_title'])
+        self.btn_close = CustomButton(self.translation['close_button'])
+        self.text_browser.setText(self.translation['help'])
