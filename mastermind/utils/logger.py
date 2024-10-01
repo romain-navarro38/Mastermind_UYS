dc592133ea9c290dc6ad5c54a2ccd85a13d9e181
romain-navarro38 <romain.navarro38@gmail.com>
2024-10-02 00:18:29 +0200

    Logger write in utf-8

--- mastermind/utils/logger.py
+++ mastermind/utils/logger.py
@@ -4,9 +4,10 @@ from .dir import Dir
 
 
 def setup_logger(name: str, level=logging.DEBUG):
+    """Génère un logger personnalisé"""
     logger = logging.getLogger(name)
     logger.setLevel(level)
-    handler = logging.FileHandler(Dir.ROOT / "app.log")
+    handler = logging.FileHandler(Dir.ROOT / "app.log", encoding='utf-8')
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
